00e0350d00a44167cf4f03489877c2bad6206687
Atuo Israel <atuoisrael3@gmail.com>
2026-09-24 15:36:09 +0100

    remove disallow: in robot.txt, add sitemap verification meta tag to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+{{--    //sitemap verification--}}
+    <meta name="google-site-verification" content="q_MB4L73f8sFlCCR2OGSB4qH25L-wyzhf5ubioP_0Rk" />
+{{--    //end of sitemap verification--}}
+
     <link rel="icon" href="/favicon.ico" sizes="any">
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
